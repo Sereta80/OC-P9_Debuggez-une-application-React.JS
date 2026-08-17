57939712452ade194ba4ffa6969a4276fa506011
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-08-17 21:10:22 +0200

    [Fix] add onSuccess function

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -51,6 +52,7 @@ const Form = ({ onSuccess, onError }) => {
       </div>
     </form>
   );
+  
 };
 
 Form.propTypes = {
