f38ae076b45fc818171d2361a31086130cd5b332
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-08-18 07:38:02 +0200

    Add comments

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,11 +13,14 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+
+  // Filter by event type
+  const eventsByType = !type
+    ? data?.events
+    : data?.events?.filter((event) => event.type === type);
+
+  // Pagination filter
+  const filteredEvents = (eventsByType || []).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
@@ -26,11 +29,14 @@ const EventList = () => {
     }
     return false;
   });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  const pageNumber = Math.ceil((filteredEvents?.length || 0) / PER_PAGE);
+
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
